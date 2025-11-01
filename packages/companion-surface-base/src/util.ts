765bd2b759faa00372b1fdda8cc5b7b4cbf137d7
Julian Waller <git@julusian.co.uk>
2025-11-01 16:02:53 +0000

    fix: drop unsupported colors mode

--- packages/companion-surface-base/src/util.ts
+++ packages/companion-surface-base/src/util.ts
@@ -2,7 +2,13 @@ export function assertNever(_v: never): void {
 	// Nothing to do
 }
 
-export function parseColor(color: string | undefined): { r: number; g: number; b: number } {
+export interface RgbColor {
+	r: number
+	g: number
+	b: number
+}
+
+export function parseColor(color: string | undefined): RgbColor {
 	const r = color ? parseInt(color.substr(1, 2), 16) : 0
 	const g = color ? parseInt(color.substr(3, 2), 16) : 0
 	const b = color ? parseInt(color.substr(5, 2), 16) : 0
